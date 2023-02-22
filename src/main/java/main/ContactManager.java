3ea856b3578f0cc6c337847a3f32d72b5311adb4
Andrea Varnado <andrea.h.varnado@gmail.com>
2023-02-22 13:35:26 -0600

    input class

--- src/main/java/main/ContactManager.java
+++ src/main/java/main/ContactManager.java
@@ -1,7 +1,22 @@
 package main;
 
+import java.util.Scanner;
+
 public class ContactManager {
     public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
 
+        while(true) {
+            //display contacts
+            showContacts();
+            //display menu
+            printMenu();
+
+            int userChoice = ;
+            //do the choices made by user
+            doChoice();
+            //exit out of the loop
+
+        }
     }
 }
