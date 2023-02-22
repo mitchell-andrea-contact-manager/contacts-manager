7a4c8bbb15aac8d5bff5a56636d665c0e205c6b5
Andrea Varnado <andrea.h.varnado@gmail.com>
2023-02-22 13:45:19 -0600

    print menu and started do choice

--- src/main/java/main/ContactManager.java
+++ src/main/java/main/ContactManager.java
@@ -1,22 +1,57 @@
 package main;
 
+import util.Input;
+
 import java.util.Scanner;
 
 public class ContactManager {
+
+    private static Input userInput = new Input();
+
     public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
 
         while(true) {
-            //display contacts
+
             showContacts();
-            //display menu
+
             printMenu();
 
-            int userChoice = ;
-            //do the choices made by user
-            doChoice();
-            //exit out of the loop
+            int userChoice = userInput.getInt();
 
+            doChoice(userChoice);
+
+            if(userChoice == 5){
+                System.out.println("See ya!");
+                break;
+            }
+        }
+
+    }
+
+    //display contacts
+    private static void showContacts() {
+    }
+
+    //print the menu
+    private static void printMenu() {
+        System.out.println("""
+                
+                1. View contacts
+                2. Add a new contact
+                3. Search a contact by name
+                4. Delete an existing contact
+                5. Exit
+                
+                Enter an option (1, 2, 3, 4, or 5):
+                """);
+    }
+
+    //do users choice
+    private static void doChoice(int choice) {
+        if(choice == 1){
+            //show contacts
+        }else if(choice == 2){
+            //add a new contact
         }
     }
 }
