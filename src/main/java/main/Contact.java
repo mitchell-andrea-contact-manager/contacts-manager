ea42d8b3b59ab054da42ff4116571b1a859b5321
mcampbell379 <campbell.mitchell45@gmail.com>
2023-02-22 13:51:52 -0600

    contacts class cont.

---
+++ src/main/java/main/Contact.java
@@ -0,0 +1,36 @@
+package main;
+
+public class Contact {
+    private String name;
+    private String number;
+
+    public Contact(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "name='" + name + '\'' +
+                ", number='" + number + '\'' +
+                '}';
+    }
+
+    // accessors
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
--- src/main/java/main/ContactManager.java
+++ src/main/java/main/ContactManager.java
@@ -4,19 +4,7 @@ import java.util.Scanner;
 
 public class ContactManager {
     public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
 
-        while(true) {
-            //display contacts
-            showContacts();
-            //display menu
-            printMenu();
 
-            int userChoice = ;
-            //do the choices made by user
-            doChoice();
-            //exit out of the loop
-
-        }
     }
 }
