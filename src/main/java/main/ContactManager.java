3b2957bb53a605fc04a5d9f6de99148da88328d4
Andrea Varnado <andrea.h.varnado@gmail.com>
2023-02-23 15:06:35 -0600

    final formatting an organizing

--- src/main/java/main/ContactManager.java
+++ src/main/java/main/ContactManager.java
@@ -9,15 +9,13 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.nio.file.Files.readAllLines;
-
 public class ContactManager {
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_PURPLE = "\u001B[35m";
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_CYAN = "\u001B[36m";
 
-    private static Input userInput = new Input();
+    private static final Input userInput = new Input();
 
     private static ArrayList<Contact> contacts;
 
@@ -25,41 +23,32 @@ public class ContactManager {
 
         getContactInfo();
 
-        while(true) {
+        while (true) {
 
             printMenu();
 
-//            System.out.println(ANSI_PURPLE);
             int userChoice = userInput.getInt();
 
             doChoice(userChoice);
 
-            if(userChoice == 5){
-                //add all new info to contacts.txt
+            if (userChoice == 5) {
 
                 System.out.println(ANSI_PURPLE + "See ya!" + ANSI_RESET);
                 writeContactInfo();
+
                 break;
             }
         }
 
     }
 
-    //display contacts
-    private static void showContacts() {
-//        System.out.println(contacts);
-        System.out.printf("""
-        Name | Phone number
-        %s
-        """,contacts);
-    }
-
     private static void getContactInfo() {
+
         Path contactsPath = Paths.get("src/main/java/data/contacts.txt");
+
         try {
             List<String> contactList = Files.readAllLines(contactsPath);
             ArrayList<Contact> contactArrayList = new ArrayList<>();
-//            System.out.println(contactList);
 
             for (String s : contactList) {
                 contactArrayList.add(Contact.createFromFileString(s));
@@ -70,6 +59,83 @@ public class ContactManager {
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
+    }
+
+    private static void printMenu() {
+        System.out.println(ANSI_CYAN + """
+                                
+                1. View contacts
+                2. Add a new contact
+                3. Search a contact by name
+                4. Delete an existing contact
+                5. Exit
+                                
+                Enter an option (1, 2, 3, 4, or 5):
+                """ + ANSI_RESET);
+    }
+
+    private static void doChoice(int choice) {
+        if (choice == 1) {
+            viewContacts();
+        } else if (choice == 2) {
+            addNewContact();
+        } else if (choice == 3) {
+            searchContact();
+        } else if (choice == 4) {
+            deleteContact();
+        }
+    }
+
+    private static void viewContacts() {
+        System.out.println("""
+                Name | Phone number
+                ------------------- """);
+        for (Contact contact : contacts) {
+            System.out.println(contact);
+        }
+    }
+
+    private static void addNewContact() {
+        String contactName = userInput.getString(ANSI_CYAN + "Enter the contacts name: ");
+        System.out.println();
+        String contactNumber = userInput.getString(ANSI_CYAN + "Enter the contacts 10 digit phone number: ");
+        System.out.println();
+
+        contacts.add(new Contact(contactName, contactNumber));
+
+        System.out.printf(ANSI_PURPLE + "%s has been added to your contacts.", contactName);
+        System.out.println();
+    }
+
+    private static Contact searchContact() {
+        System.out.println(ANSI_PURPLE + "Search: " + ANSI_RESET);
+        String userSearch = userInput.getString();
+        System.out.println();
+        for (Contact contact : contacts) {
+            if (contact.getName().toLowerCase().startsWith(userSearch.toLowerCase())) {
+                System.out.println(contact);
+                System.out.println();
+                boolean searchAgain = userInput.yesNo(ANSI_CYAN + "Is " + contact.getName() + " the correct contact?" + ANSI_RESET);
+                System.out.println();
+                if (!searchAgain) {
+                    Contact temp = searchContact();
+                    if (temp != null) {
+                        contact = temp;
+                    }
+                }
+                return contact;
+            }
+        }
+        return null;
+    }
+
+    private static void deleteContact() {
+
+        System.out.print(ANSI_CYAN + "What is the name of the contact you would like to delete? \n" + ANSI_RESET);
+        Contact userContactRemoveSelect = searchContact();
+
+        System.out.printf(ANSI_RED + "%s has been deleted!" + ANSI_RESET, userContactRemoveSelect);
+        contacts.remove(userContactRemoveSelect);
     }
 
     private static void writeContactInfo() {
@@ -85,88 +151,6 @@ public class ContactManager {
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
-    }
-
-    //print the menu
-    private static void printMenu() {
-        System.out.println(ANSI_CYAN + """
-                
-                1. View contacts
-                2. Add a new contact
-                3. Search a contact by name
-                4. Delete an existing contact
-                5. Exit
-                
-                Enter an option (1, 2, 3, 4, or 5):
-                """ + ANSI_RESET);
-    }
-
-    //display contacts
-    private static void viewContacts() {
-        System.out.println( """
-                Name | Phone number
-                ------------------- """ );
-        for (Contact contact : contacts) {
-            System.out.println(contact);
-        }
-    }
-
-    //do users choice
-    private static void doChoice(int choice) {
-        if(choice == 1){
-            viewContacts();
-        }else if(choice == 2){
-            addNewContact();
-        }else if(choice == 3){
-            searchContact();
-        }else if(choice == 4){
-            deleteContact();
-        }
-    }
-
-
-    private static void addNewContact() {
-        //get contact info from user
-        String contactName = userInput.getString(ANSI_CYAN + "Enter the contacts name: ");
-        System.out.println();
-        String contactNumber = userInput.getString(ANSI_CYAN + "Enter the contacts 10 digit phone number: ");
-        System.out.println();
-        //convert user input into string that can be plugged into file data
-
-        contacts.add(new Contact(contactName,contactNumber));
-
-        System.out.printf(ANSI_PURPLE+ "%s has been added to your contacts.",contactName);
-        System.out.println();
-    }
-
-    private static Contact searchContact() {
-        System.out.println(ANSI_PURPLE+"Search: " + ANSI_RESET);
-        String userSearch = userInput.getString();
-        System.out.println();
-        for (Contact contact : contacts) {
-            if(contact.getName().toLowerCase().startsWith(userSearch.toLowerCase())) {
-                System.out.println(contact);
-                System.out.println();
-                boolean searchAgain = userInput.yesNo(ANSI_CYAN+"Would you like to search again?" +ANSI_RESET);
-                System.out.println();
-                if(searchAgain){
-                    Contact temp = searchContact();
-                    if(temp != null) {
-                        contact = temp;
-                    }
-                }
-                return contact;
-            }
-        }
-        return null;
-    }
-
-    private static void deleteContact() {
-        System.out.print(ANSI_CYAN+"What is the name of the contact you would like to delete? \n" + ANSI_RESET);
-        //convert user input into string that can be plugged into file data
-        Contact userContactRemoveSelect = searchContact();
-        System.out.println(userContactRemoveSelect);
-        contacts.remove(userContactRemoveSelect);
     }
 
 }
